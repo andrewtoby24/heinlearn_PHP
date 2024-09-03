d34d5e913937af43e98756157593c6770b7ef178
andrewtoby24 <thantkaung501@gmail.com>
2024-09-03 23:15:28 +0800

    PHP Day 3

--- variable_and_string.php
+++ variable_and_string.php
@@ -13,4 +13,47 @@
     echo 'I live in '.$address;
     echo '<br>';
     echo $name.'. I live in '.$address;
+
+    // .=
+    $first_name = "Alan";
+    echo "<p>$first_name</p>";
+    $last_name = "Walker";
+    echo "<p>$last_name</p>";
+
+    $first_name = "Walker";
+    echo "<p>$first_name</p>";
+
+    // trim() => remove first and last space
+    $singer = ' Taylor Swift ';
+    echo "<p>My favourite singer is $singer.</p>";
+    echo "<p>My favourite singer is ".trim($singer).".</p>";
+
+    // strlen() => string length
+    echo strlen($singer);
+    echo "<br>";
+    $singer_length = trim($singer);
+    echo strlen($singer_length);
+    echo "<br>";
+
+    // strtolower(string) => change to lowercase
+    echo strtolower($singer);
+    echo "<br>";
+
+    // strtoupper(string) => change to uppercase
+    echo strtoupper($singer);
+    echo "<br>";
+
+    // substr(string,start(0), number)  => substring
+    echo substr("Hello HeinLearner",6);
+    echo "<br>";
+    echo substr("Hello HeinLearner",6,4);
+    echo "<br>";
+    echo substr("Hello HeinLearner",10,5);
+    echo "<br>";
+
+    // strreplace(search, replace, subject);
+    $city = "Hello Mandalay";
+    echo "<p>$city</p>";
+    echo str_replace('Mandalay','HtoeMont',$city);
+
 ?>
