f66d5a2993ec2dd9b552db00c7a9d3fb5a952fc6
andrewtoby24 <thantkaung501@gmail.com>
2024-09-05 22:58:33 +0800

    PHP Day 5

--- number.php
+++ number.php
@@ -11,9 +11,26 @@
     echo $number1;
     echo "<br>";
     echo ++$number2; // 1 + $number2
-    echo "<br>"
+    echo "<br>";
 
     $number1 += $number2; // $number1 = $number1 + $number2
     echo $number1;
+
+    $firstNumber = 600;
+    $secondNumber = 400;
+    $add = $firstNumber + $secondNumber;
+    echo "<p>$add</p>";
+
+    $sub = $firstNumber - $secondNumber;
+    echo "<p>$sub</p>";
+
+    $mul = $firstNumber * $secondNumber;
+    echo "<p>$mul</p>";
+
+    $div = $firstNumber / $secondNumber;
+    echo "<p>$div</p>";
+
+    $mod = $firstNumber % $secondNumber;
+    echo "<p>$mod</p>";
 
 ?>
