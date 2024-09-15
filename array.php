f6394fa97a6a143e290632360320fafeb153760a
andrewtoby24 <thantkaung501@gmail.com>
2024-09-15 14:44:47 +0630

    PHP Lessons

---
+++ array.php
@@ -0,0 +1,17 @@
+<?php
+
+    //index Array
+    $laptops = array("Apple","Samsung","MSI","Asus","Alienware","HP");
+    print_r($laptops);
+    echo "<br>";
+    echo $laptops[2];
+
+    echo "<br>";
+
+    $fruits = [];
+    $fruits[0] = "Apple";
+    $fruits[1] = "Orange";
+    $fruits[2] = "Lemon";
+    $fruits[3] = "Mango";
+    print_r($fruits);
+?>
