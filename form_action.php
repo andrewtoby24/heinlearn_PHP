03128959a329fa210b25fc634b3c06b6f5698109
andrewtoby24 <thantkaung501@gmail.com>
2024-09-22 21:05:02 +0630

    PHP Lessons

--- form_action.php
+++ form_action.php
@@ -1,6 +1,20 @@
 <?php
     var_dump($_REQUEST);
-    // Super Global Variables
+    // var_dump($_SERVER);
+    // var_dump($_GET);
+    // var_dump($_POST);
+    // var_dump($_FILES);
+    // Super Global Variables start with $_
     // $_REQUEST , $_SERVER , $_GET , $_POST , $_FILES
     // $_FILES for carrying the files 
+
+    $name = $_GET["name"];
+    $email = $_GET["email"];
+    $password = $_GET["password"];
+
+    echo "<h1>$name</h1>";
+    echo "<h1>$email</h1>";
+    echo "<h1>$password</h1>";
+
+
 ?>
